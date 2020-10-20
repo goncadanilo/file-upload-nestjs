99a280e4f2d9e6974581e6d9df3ea329266bcc28
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-10-20 11:09:01 -0300

    test: should create a post and return it

--- src/modules/posts/controller/posts.controller.spec.ts
+++ src/modules/posts/controller/posts.controller.spec.ts
@@ -1,11 +1,16 @@
 import { Test } from '@nestjs/testing';
+import { TestUtil } from '../../../../test/util/test.util';
+import { Posts } from '../entity/posts.entity';
 import { PostsService } from '../service/posts.service';
 import { PostsController } from './posts.controller';
 
 describe('PostsController', () => {
   let postsController: PostsController;
+  let mockPost: Posts;
 
-  const mockPostsService = {};
+  const mockPostsService = {
+    createPost: jest.fn(),
+  };
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -14,9 +19,33 @@ describe('PostsController', () => {
     }).compile();
 
     postsController = moduleRef.get<PostsController>(PostsController);
+    mockPost = TestUtil.getMockPost();
+  });
+
+  beforeEach(() => {
+    mockPostsService.createPost.mockReset();
   });
 
   it('should be defined', () => {
     expect(postsController).toBeDefined();
+  });
+
+  describe('when create post', () => {
+    it('should create a post and return it', async () => {
+      mockPostsService.createPost.mockReturnValue(mockPost);
+
+      const post = {
+        description: mockPost.description,
+        hashtags: mockPost.hashtags,
+      };
+
+      const createdPost = await postsController.createPost(
+        mockPost.image,
+        post,
+      );
+
+      expect(createdPost).toHaveProperty('id', 1);
+      expect(createdPost).toMatchObject(mockPost);
+    });
   });
 });
