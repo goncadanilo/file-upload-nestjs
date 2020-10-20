5861efeaba90a72aeb2ebae5d0dfb8b1db19b8a0
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-10-20 09:54:34 -0300

    chore: create multer config

--- src/modules/posts/posts.module.ts
+++ src/modules/posts/posts.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MulterConfig } from 'src/shared/config/multer.config';
 import { PostsController } from './controller/posts.controller';
 import { Posts } from './entity/posts.entity';
 import { PostsService } from './service/posts.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Posts])],
+  imports: [
+    TypeOrmModule.forFeature([Posts]),
+    MulterModule.registerAsync({
+      useClass: MulterConfig,
+    }),
+  ],
   providers: [PostsService],
   controllers: [PostsController],
 })
